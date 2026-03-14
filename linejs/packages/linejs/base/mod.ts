c2f49388b9fc60ef654f27dc34d54f769dac5186
Wonder <imagination_2011@icloud.com>
2026-03-14 18:11:39 +0900

    Update mod.ts

--- linejs/packages/linejs/base/mod.ts
+++ linejs/packages/linejs/base/mod.ts
@@ -3,5 +3,5 @@
  * @module
  */
 
-export * from "https://raw.githubusercontent.com/wonderhax/wonder-line/main/C:/Users/Admin/Documents/work/wonder-line/linejs/packages/linejs/base/core/mod.ts";
-export * from "https://raw.githubusercontent.com/wonderhax/wonder-line/main/C:/Users/Admin/Documents/work/wonder-line/linejs/packages/linejs/base/types.ts";
+export * from "https://raw.githubusercontent.com/wonderhax/wonder-line/main/linejs/packages/linejs/base/core/mod.ts
+export * from "https://raw.githubusercontent.com/wonderhax/wonder-line/main/linejs/packages/linejs/base/types.ts";
