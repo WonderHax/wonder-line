7fad39fa431cba2250a60a20a69e242144b01d3a
Wonder <imagination_2011@icloud.com>
2026-03-14 17:57:33 +0900

    Update login.ts

--- linejs/packages/linejs/client/login.ts
+++ linejs/packages/linejs/client/login.ts
@@ -5,7 +5,8 @@
 
 import type { FetchLike } from "https://raw.githubusercontent.com/wonderhax/wonder-line/main/C:/Users/Admin/Documents/work/wonder-line/linejs/packages/linejs/base/mod.ts";
 import type { Device } from "https://raw.githubusercontent.com/wonderhax/wonder-line/main/C:/Users/Admin/Documents/work/wonder-line/linejs/packages/linejs/base/mod.ts";
-import { BaseClient } from "https://raw.githubusercontent.com/wonderhax/wonder-line/main/C:/Users/Admin/Documents/work/wonder-line/linejs/packages/linejs/base/mod.ts";
+import { BaseClient } from "https://raw.githubusercontent.com/wonderhax/wonder-line/main/linejs/packages/linejs/base/mod.ts";
+
 import type { BaseStorage } from "https://raw.githubusercontent.com/wonderhax/wonder-line/main/C:/Users/Admin/Documents/work/wonder-line/linejs/packages/linejs/base/storage/mod.ts";
 import { Client } from "https://raw.githubusercontent.com/wonderhax/wonder-line/main/C:/Users/Admin/Documents/work/wonder-line/linejs/packages/linejs/client/client.ts";
 
